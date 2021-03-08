343003f61426c78c3919b5215655fde58f8a98a0
= <kthompson395@hotmail.com>
2021-03-08 01:22:18 -0600

    need to some work on map projections

---
+++ src/README.md
@@ -0,0 +1,7 @@
+# Projections Section
+
+Maps must be projected because the Earth is actually not flat. I know that a controversy for some people these days, but it is what it is. 
+
+We have to pick a projection based on the size of the region we want to show and how precise we need to be. 
+
+Here is a good guide on [projections](https://source.opennews.org/articles/choosing-right-map-projection/#poison). 
--- src/app.jl
+++ src/app.jl
@@ -1,7 +1,29 @@
-using Dash, DashHtmlComponents, DashCoreComponents
+using Dash, DashHtmlComponents, DashCoreComponents, GeoJSON
+using WGLMakie, GeoMakie, GeoMakie.GeoInterface, GeoMakie.GeoJSON
 
-app = dash()
 
-app.layout = html_div()
+begin
+    source = LonLat()
+    dest = WinkelTripel()
+    states = download("https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json")
+    states_geo = GeoJSON.parse(read(states, String))
+    lons = LinRange(-179.5, 179.5, 360 ÷ 2)
+    lats = LinRange(-89.5, 89.5, 180 ÷ 2)
+    field = [exp(cosd(l)) + 3(y/90) for l in lons, y in lats]
+    fig = Figure()
+    ax = fig[1,1] = Axis(fig)
+    ax.scene.transformation.transform_func[] = GeoMakie.proj(source, dest)
+    wireframe!(ax, lons, lats, field, color=(:gray, 0.2), transparency=true)
+    n = length(GeoInterface.features(states_geo))
+    lines!(ax, GeoMakie.coastlines())
+    poly!(ax, states_geo, color= 1:n, strokecolor = :blue, strokewidth = 1)
+    display(fig)
+end
 
-run_server(app, "0.0.0.0", debug=true)
+
+
+#app = dash()
+
+#app.layout = html_div()
+
+#run_server(app, "0.0.0.0", debug=true)
